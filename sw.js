11e446fb2be099ecd25cac1588a8b466fd72c471
Emil-AR-storm <Emil@stormentreprenor.no>
2026-09-25 13:51:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@
 
 // ---- GENERERT BLOKK START (verktoy/lag-sw-liste.mjs) ----
 // IKKE REDIGER FOR HÅND. Kjør: node "verktoy/lag-sw-liste.mjs"
-const SW_VERSJON = "f92aeed656bf";
+const SW_VERSJON = "25f77f03f360";
 const SKALL = [
   "/bygg.html",
   "/css/storm.css",
